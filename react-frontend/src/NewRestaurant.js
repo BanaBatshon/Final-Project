f830af25935ba7bfea24af72f418e62c50c5795c
Bana <banabatshon@hotmail.com>
2019-04-30 20:26:32 +0000

    modify handle submit func to post a json body to backend route. npm install superagent and cors

--- react-frontend/src/NewRestaurant.js
+++ react-frontend/src/NewRestaurant.js
@@ -1,69 +1,86 @@
 import React, { Component } from 'react';
+var superagent = require('superagent');
 // import { connect } from 'react-redux'
 
 class NewRestaurantForm extends Component {
   state =  {
-    Name: '',
-    Address: '',
-    Tags: '',
-    Website: '',
-    Description: '',
-    submit: 'submit'
+    name: '',
+    address: '',
+    phone_number: '',
+    website: '',
+    description: ''
   }
 
+ 
+ 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit = (e) => {
-    e.preventefault();
-    this.setstate({ ['submit']: 'submitted'})
+
+    console.log('form submission event:', e)
+    e.preventDefault();
+
+    superagent
+      .post('http://localhost:3001/restaurants')
+      .send({name: this.state.name, address: this.state.address, phone_number: this.state.phone_number, website: this.state.website, description: this.state.description, approved: false}) // sends a JSON post body
+      .set('accept', 'json')
+      .end((err, res) => {
+        if (err) {
+          console.log("An Error Occurred")
+        }
+        else {
+        console.log(res);
+        }
+      });
+
   }
 
   render() {
     return (
       
-      <div class="col-md-12 col-lg-8 mb-5">
-            <form action="#" class="p-5 bg-white">
+      <div className="col-md-12 col-lg-8 mb-5">
+            <form action="#" className="p-5 bg-white" onSubmit={this.handleSubmit}>
 
-              <div class="row form-group">
-                <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="font-weight-bold" for="fullname">Name</label>
-                  <input name="Name" value={this.state.Name} onChange={this.handleChange} type="text" id="fullname" class="form-control" placeholder="Name"/>
+              <div className="row form-group">
+                <div className="col-md-12 mb-3 mb-md-0">
+                  <label className="font-weight-bold" htmlFor="fullname">Name</label>
+                  <input name="name" value={this.state.name} onChange={this.handleChange} type="text" id="fullname" className="form-control" placeholder="Name"/>
                 </div>
               </div>
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="font-weight-bold" for="text">Address</label>
-                  <input name="Address" value={this.state.Address} onChange={this.handleChange} type="text" id="text" class="form-control" placeholder="eg. 1234 Main Street, Vancouver"/>
-                </div>
-              </div>
-
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="font-weight-bold" for="text">Tags</label>
-                  <input name="Tags" value={this.state.Tags} onChange={this.handleChange} type="text" id="text" class="form-control" placeholder=""/>
+              <div className="row form-group">
+                <div className="col-md-12">
+                  <label className="font-weight-bold" htmlFor="text">Address</label>
+                  <input name="address" value={this.state.address} onChange={this.handleChange} type="text" id="text" className="form-control" placeholder="eg. 1234 Main Street, Vancouver"/>
                 </div>
               </div>
 
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="font-weight-bold" for="email">Website</label>
-                  <input name="Website" value={this.state.Website} onChange={this.handleChange} type="email" id="text" class="form-control" placeholder="https://www.example.com"/>
+              <div className="row form-group">
+                <div className="col-md-12">
+                  <label className="font-weight-bold" htmlFor="text">Phone Number</label>
+                  <input name="phone_number" value={this.state.phone_number} onChange={this.handleChange} type="text" id="text" className="form-control" placeholder=""/>
                 </div>
               </div>
 
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="font-weight-bold" for="message">Description</label>
-                  <textarea name="Description" value={this.state.Description} onChange={this.handleChange} id="message" cols="30" rows="5" class="form-control"
+              <div className="row form-group">
+                <div className="col-md-12">
+                  <label className="font-weight-bold" htmlFor="text">Website</label>
+                  <input name="website" value={this.state.website} onChange={this.handleChange} type="text" id="text" className="form-control" placeholder="https://www.example.com"/>
+                </div>
+              </div>
+
+              <div className="row form-group">
+                <div className="col-md-12">
+                  <label className="font-weight-bold" htmlFor="message">Description</label>
+                  <textarea name="description" value={this.state.description} onChange={this.handleChange} id="message" cols="30" rows="5" className="form-control"
                     placeholder="Description"></textarea>
                 </div>
               </div>
 
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <input type="submit" value={this.state.submit} onSubmit={this.handleSubmit} class="btn btn-primary  py-2 px-4"/>
+              <div className="row form-group">
+                <div className="col-md-12">
+                  <input type="submit" value={this.state.submit} className="btn btn-primary  py-2 px-4"/>
                 </div>
               </div>
 
@@ -73,5 +90,4 @@ class NewRestaurantForm extends Component {
     )
   }
 }
-
 export default NewRestaurantForm;
