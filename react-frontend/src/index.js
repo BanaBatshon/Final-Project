004a36b6bd0c33c606687c28cdf4fac551b97bca
Andrew Li <mail@andrewli.ca>
2019-04-30 17:41:14 +0000

    Setup homepage components

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import App from './components/App';
 import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { applyMiddleware, createStore } from 'redux';
 
-import configureStore from './configureStore';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const defaultState = {
+  appName: 'foodie',
+  restaurants: null
+};
+const reducer = function(state = defaultState, action) {
+  return state;
+};
+const store = createStore(reducer);
 
-// Create redux store
-const store = configureStore();
-
-console.log(store)
-
-ReactDOM.render(
+ReactDOM.render((
   <Provider store={store}>
     <App />
   </Provider>
-, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+), document.getElementById('root'));
