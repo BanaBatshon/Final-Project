b921a97c06a71dc6142fd7b19b0989c3036ec009
Bana <banabatshon@hotmail.com>
2019-05-01 19:48:16 +0000

    import newRestaurant form from redux-form instead of creating another form reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import restaurants from './restaurantReducer';
-import newRestaurant from './newRestaurantReducer';
+import newRestaurant from 'redux-form';
 
 export default combineReducers({
     restaurants: restaurants,
