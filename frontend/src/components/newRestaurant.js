47a6e527749a9f32e0f95c25de39ba3e9a1d3bbf
Bana <banabatshon@hotmail.com>
2019-05-01 23:56:23 +0000

    Modify the 'classes' and 'for' names

--- frontend/src/components/newRestaurant.js
+++ frontend/src/components/newRestaurant.js
@@ -16,48 +16,48 @@ let NewRestaurantForm = props => {
   const { handleSubmit } = props
   return (
     <form onSubmit={handleSubmit}>
-      <div class="col-md-12 col-lg-8 mb-5">
-        <div action="#" class="p-5 bg-white">
+      <div className="col-md-12 col-lg-8 mb-5">
+        <div action="#" className="p-5 bg-white">
 
-          <div class="row form-group">
-            <div class="col-md-12 mb-3 mb-md-0">
-              <label class="font-weight-bold" for="fullname">Name</label>
-              <Field name="fullname" component="input" type="text" id="fullname" class="form-control" placeholder="Name"/>
+          <div className="row form-group">
+            <div className="col-md-12 mb-3 mb-md-0">
+              <label className="font-weight-bold" htmlFor="fullname">Name</label>
+              <Field name="fullname" component="input" type="text" id="fullname" className="form-control" placeholder="Name"/>
             </div>
           </div>
 
-          <div class="row form-group">
-            <div class="col-md-12">
-              <label class="font-weight-bold" for="email">Address</label>
-              <Field name="address" component="input" type="email" id="email" class="form-control" placeholder="eg. 1234 Main Street, Vancouver"/>
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="email">Address</label>
+              <Field name="address" component="input" type="email" id="email" className="form-control" placeholder="eg. 1234 Main Street, Vancouver"/>
             </div>
           </div>
 
-          <div class="row form-group">
-            <div class="col-md-12">
-              <label class="font-weight-bold" for="email">Tags</label>
-              <Field name="tags" component="input" type="email" id="email" class="form-control" placeholder=""/>
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="email">Tags</label>
+              <Field name="tags" component="input" type="email" id="email" className="form-control" placeholder=""/>
             </div>
           </div>
 
-          <div class="row form-group">
-            <div class="col-md-12">
-              <label class="font-weight-bold" for="email">Website</label>
-              <Field name="website" component="input" type="email" id="email" class="form-control" placeholder="https://www.example.com"/>
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="email">Website</label>
+              <Field name="website" component="input" type="email" id="email" className="form-control" placeholder="https://www.example.com"/>
             </div>
           </div>
 
-          <div class="row form-group">
-            <div class="col-md-12">
-              <label class="font-weight-bold" for="message">Description</label>
-              <Field name="description" component="textarea" id="message" cols="30" rows="5" class="form-control"
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="message">Description</label>
+              <Field name="description" component="textarea" id="message" cols="30" rows="5" className="form-control"
                 placeholder="Description"/>
             </div>
           </div>
 
-          <div class="row form-group">
-            <div class="col-md-12">
-              <button type="submit" class="btn btn-primary  py-2 px-4">Submit</button>
+          <div className="row form-group">
+            <div className="col-md-12">
+              <button type="submit" className="btn btn-primary  py-2 px-4">Submit</button>
             </div>
           </div>
 
