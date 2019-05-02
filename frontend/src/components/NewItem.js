af2af9514dff918ea7cb50d626d4b4e3c3efde49
Bana <banabatshon@hotmail.com>
2019-05-02 01:03:33 +0000

    Update the form fields

--- frontend/src/components/NewItem.js
+++ frontend/src/components/NewItem.js
@@ -23,43 +23,28 @@ let NewItemForm = props => {
 
           <div className="row form-group">
             <div className="col-md-12 mb-3 mb-md-0">
-              <label className="font-weight-bold" htmlFor="name">Name</label>
-              <Field name="name" component="input" type="text" id="fullname" className="form-control" placeholder="Name"/>
+              <label className="font-weight-bold" htmlFor="name">Restaurant</label>
+              <Field name="name" component="input" type="text" id="name" className="form-control" placeholder="Search by restaurant name"/>
             </div>
           </div>
 
           <div className="row form-group">
             <div className="col-md-12">
-              <label className="font-weight-bold" htmlFor="email">Address</label>
-              <Field name="address" component="input" type="email" id="email" className="form-control" placeholder="eg. 1234 Main Street, Vancouver"/>
+              <label className="font-weight-bold" htmlFor="email">Dish</label>
+              <Field name="item" component="input" type="email" id="email" className="form-control" placeholder="eg. Pepperoni Pizza"/>
             </div>
           </div>
 
           <div className="row form-group">
             <div className="col-md-12">
               <label className="font-weight-bold" htmlFor="email">Tags</label>
-              <Field name="tags" component="input" type="email" id="email" className="form-control" placeholder=""/>
+              <Field name="tags" component="input" data-role="tagsinput" type="email" id="email" className="form-control" placeholder=""/>
             </div>
           </div>
 
           <div className="row form-group">
             <div className="col-md-12">
-              <label className="font-weight-bold" htmlFor="email">Website</label>
-              <Field name="website" component="input" type="email" id="email" className="form-control" placeholder="https://www.example.com"/>
-            </div>
-          </div>
-
-          <div className="row form-group">
-            <div className="col-md-12">
-              <label className="font-weight-bold" htmlFor="message">Description</label>
-              <Field name="description" component="textarea" id="message" cols="30" rows="5" className="form-control"
-                placeholder="Description"/>
-            </div>
-          </div>
-
-          <div className="row form-group">
-            <div className="col-md-12">
-              <button type="submit" className="btn btn-primary  py-2 px-4">Submit</button>
+              <button type="submit" className="btn btn-primary  py-2 px-4">Add Dish</button>
             </div>
           </div>
 
