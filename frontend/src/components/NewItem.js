0fb0cfecb3045682e20af8bec9002997d1c48571
Bana <banabatshon@hotmail.com>
2019-05-02 00:26:48 +0000

    Create a new item form component

---
+++ frontend/src/components/NewItem.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+import axios from 'axios'
+
+const NewItem = () => {
+  const onSubmit = ((values) => {
+    axios
+    .post('http://localhost:3001/items', values)
+  })
+  return (
+    <div>
+      <NewItemForm onSubmit={onSubmit}/>
+    </div>
+  )
+}
+
+let NewItemForm = props => {
+  const { handleSubmit } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="col-md-12 col-lg-8 mb-5">
+        <div action="#" className="p-5 bg-white">
+
+          <div className="row form-group">
+            <div className="col-md-12 mb-3 mb-md-0">
+              <label className="font-weight-bold" htmlFor="name">Name</label>
+              <Field name="name" component="input" type="text" id="fullname" className="form-control" placeholder="Name"/>
+            </div>
+          </div>
+
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="email">Address</label>
+              <Field name="address" component="input" type="email" id="email" className="form-control" placeholder="eg. 1234 Main Street, Vancouver"/>
+            </div>
+          </div>
+
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="email">Tags</label>
+              <Field name="tags" component="input" type="email" id="email" className="form-control" placeholder=""/>
+            </div>
+          </div>
+
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="email">Website</label>
+              <Field name="website" component="input" type="email" id="email" className="form-control" placeholder="https://www.example.com"/>
+            </div>
+          </div>
+
+          <div className="row form-group">
+            <div className="col-md-12">
+              <label className="font-weight-bold" htmlFor="message">Description</label>
+              <Field name="description" component="textarea" id="message" cols="30" rows="5" className="form-control"
+                placeholder="Description"/>
+            </div>
+          </div>
+
+          <div className="row form-group">
+            <div className="col-md-12">
+              <button type="submit" className="btn btn-primary  py-2 px-4">Submit</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </form>
+
+  )}
+
+NewItemForm = reduxForm({
+  form: 'newItem'
+})(NewItemForm)
+
+export default NewItem;
