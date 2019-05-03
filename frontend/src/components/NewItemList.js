e1b75759ea426140f2b95668ddfd996bf37e7ef5
Bana <banabatshon@hotmail.com>
2019-05-03 17:18:18 +0000

    Create an onSubmit handler and change the function to a calss

--- frontend/src/components/NewItemList.js
+++ frontend/src/components/NewItemList.js
@@ -1,61 +1,61 @@
 import React from 'react'
-// import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux';
 import axios from 'axios'
+import MenuItemRow from './MenuItemRow';
+import { Component } from 'react';
 
-const NewItemList = () => {
-  const onSubmit = ((values) => {
-    axios
-    .post('http://localhost:3001/items', values)
-  })
-  return (
-    <div>
-      <NewItemListTable onSubmit={onSubmit}/>
-    </div>
-  )
+class NewItemListTable extends Component {
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    for ( let k = 0; k < this.props.newItems.menuItem.length; k++ ) {
+      this.props.newItems.menuItem[0].tags = [this.props.newItems.menuItem[0].tags];
+      this.props.newItems.menuItem[0].restaurantId=1;
+      axios
+      .post ('http://localhost:3001/items', (this.props.newItems.menuItem[k]) )
+    }
+  }
+  render() {
+    if (this.props.newItems.menuItem.length) {
+      return (
+        <table>
+          <div className="table table-sm">
+            <thead>
+              <tr>
+                <th class="w-5">#</th>
+                <th class="w-25">Dish</th>
+                <th class="w-25">Tags</th>
+                <th class="w-15"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.newItems.menuItem.map((item) => {
+                return (
+                  <MenuItemRow name={item.name} tags={item.tags}/>
+                )}
+              )}
+            </tbody>
+          </div>
+
+          <div className="row form-group">
+            <div className="col-md-12">
+              <button onClick={this.handleOnSubmit} className="btn btn-primary  py-2 px-4">Submit</button>
+            </div>
+          </div>
+        </table>
+      )
+    }
+    else {
+      return <div></div>
+    }
+  }
 }
 
-let NewItemListTable = props => {
-  const { handleSubmit } = props
-  return (
-    <table onSubmit={handleSubmit}>
-      <div className="table table-sm">
-        <thead>
-          <tr>
-            <th class="w-5">#</th>
-            <th class="w-25">Dish</th>
-            <th class="w-25">Tags</th>
-            <th class="w-15"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Capricciosa</td>
-            <td>
-              <span class="badge badge-success">Italian</span>
-              <span class="badge badge-success">Pizza</span>
-            </td>
-            <td>
-              <button type="button" class="btn btn-secondary btn-sm">Edit</button>
-              <button type="button" class="btn btn-danger btn-sm">Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Prosciutto e Rucola</td>
-            <td><span class="badge badge-success">Italian</span></td>
-            <td>
-              <button type="button" class="btn btn-secondary btn-sm">Edit</button>
-              <button type="button" class="btn btn-danger btn-sm">Delete</button>
-            </td>
-          </tr>
-        </tbody>
-      </div>
-    </table>
-  )}
-
-NewItemList = reduxForm({
-  form: 'newItemList'
-})(NewItemList)
-
-export default NewItemList;
+const mapStateToProps = state => {
+  return {
+    newItems: state.newItems
+  }
+}
+  
+export default connect(
+  mapStateToProps
+)(NewItemListTable);
