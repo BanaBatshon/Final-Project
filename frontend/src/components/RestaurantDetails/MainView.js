dd01e4ab636a00002f8d45bae1bddd42fe1dbf3c
Andrew Li <mail@andrewli.ca>
2019-05-03 05:59:02 +0000

    Create a new component to display a summary for the restaurant in the details view

--- frontend/src/components/RestaurantDetails/MainView.js
+++ frontend/src/components/RestaurantDetails/MainView.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Summary from './Summary';
 
-const MainView = () => {
+const MainView = (props) => {
+  const { restaurant } = props;
 
   return (
     <div className="site-section bg-light">
       <div className="container">
         <div className="row">
           <div className="bg-white col-md-12 col-lg-8 mb-5">
+            <Summary details={restaurant} />
             <div className="container p-0 pb-5">
               <div className="row justify-content-start text-left"></div>
               <div className="col-md-9 px-0 pb-4" data-aos="fade">
