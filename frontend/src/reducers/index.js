b1d7a9894a7ceab18cb6ede40f23f9757c522e47
Bana <banabatshon@hotmail.com>
2019-05-03 17:19:03 +0000

    Combine fetchNewItems reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import restaurants from './restaurantReducer';
 import menuItem from './menuItemReducer';
+import fetchNewItems from './fetchNewItemsReducer';
 import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
     restaurants: restaurants,
     newItems: menuItem,
+    fetchNewItems: fetchNewItems,
     form: formReducer
 });
