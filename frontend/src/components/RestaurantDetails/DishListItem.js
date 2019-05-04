6c5135fb3efa860a346a10ef050f71e05d2757ff
Andrew Li <mail@andrewli.ca>
2019-05-04 02:41:22 +0000

    Refactor component

--- frontend/src/components/RestaurantDetails/DishListItem.js
+++ frontend/src/components/RestaurantDetails/DishListItem.js
@@ -9,7 +9,7 @@ export default ({ dish: { name, menuitemratings }, index}) => {
 
           <div className="mb-4 mb-md-0 mr-5">
             <div className="list-item-header d-flex align-items-center">
-              <h2 className="mr-3 text-black h4"><strong>{index + 1}. </strong>{ name }</h2>
+              <h2 className="mr-3 text-black h4"><strong>{index != null ? `${index + 1}.` : ''} </strong>{ name }</h2>
               <div className="badge-wrap">
                 <span className="bg-primary text-white badge py-2 px-4">Pizza</span>
                 <span className="bg-primary text-white badge py-2 px-4">Italian</span>
