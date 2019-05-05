2ce4951d7366bcbee7123ec2d0a81630172d2825
Bana <banabatshon@hotmail.com>
2019-05-05 23:50:12 +0000

    Fix class names and wrap the table with fragment

--- frontend/src/components/NewItemList.js
+++ frontend/src/components/NewItemList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import MenuItemRow from './MenuItemRow';
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import { postToServer } from '../util/index.js';
 
 class NewItemListTable extends Component {
@@ -12,31 +12,34 @@ class NewItemListTable extends Component {
   render() {
     if (this.props.newItems.menuItem.length) {
       return (
-        <table>
+        <Fragment>
           <div className="table table-sm">
-            <thead>
-              <tr>
-                <th class="w-5">#</th>
-                <th class="w-25">Dish</th>
-                <th class="w-25">Tags</th>
-                <th class="w-15"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.newItems.menuItem.map((item) => {
-                return (
-                  <MenuItemRow name={item.name} tags={item.tags}/>
+            <table>
+              <thead>
+                <tr>
+                  <th className="w-5">#</th>
+                  <th className="w-25">Dish</th>
+                  <th className="w-25">Tags</th>
+                  <th className="w-15"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.newItems.menuItem.map((item, index) => {
+                  return (
+                    <MenuItemRow key={ index } name={item.name} tags={item.tags}/>
+                  )}
                 )}
-              )}
-            </tbody>
-          </div>
-
-          <div className="row form-group">
-            <div className="col-md-12">
-              <button onClick={this.handleOnSubmit} className="btn btn-primary  py-2 px-4">Submit</button>
+              </tbody>
+              </table>
             </div>
+
+            <div className="row form-group">
+              <div className="col-md-12">
+                <button onClick={this.handleOnSubmit} className="btn btn-primary  py-2 px-4">Submit</button>
+              </div>
           </div>
-        </table>
+        </Fragment>
+
       )
     }
     else {
