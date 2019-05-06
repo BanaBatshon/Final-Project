3b92ddd03c84a8e901d50d9df8f81605811e56de
Bana <banabatshon@hotmail.com>
2019-05-06 22:58:50 +0000

    Create a function to set restaurants in main container

--- frontend/src/components/AddNewItem.js
+++ frontend/src/components/AddNewItem.js
@@ -10,12 +10,14 @@ class AddNewItem extends Component {
   constructor(props) {
     super(props)
 
+    this.restaurants = [];
     this.tags = [];
   }
   
   handleSubmit = (values) => {
     let newItem = Object.assign({}, values);
-    newItem.restaurantId = Number(newItem.restaurantId) //delete later
+    newItem.restaurantId = this.restaurants[0].restaurantId;
+    newItem.approved = false;
     newItem.tags= this.tags;
     this.props.dispatch(addNewItemToNewItemList(newItem))
     this.props.dispatch(reset('newItem'));
@@ -24,10 +26,15 @@ class AddNewItem extends Component {
   setTags = (tags) => {
     this.tags = tags
   }
+
+  setRestaurants = (restaurants) => {
+    this.restaurants = restaurants
+  }
+  
   render() {
     return (
       <Fragment>
-        <NewItemForm setTags={ this.setTags} onSubmit={ this.handleSubmit }/>
+        <NewItemForm setTags={ this.setTags } setRestaurants={ this.setRestaurants } onSubmit={ this.handleSubmit }/>
         <NewItemList />
       </Fragment>
     );
