11164a77592f5d4095ab740894da72c86eba17c6
Bana <banabatshon@hotmail.com>
2019-05-06 00:26:47 +0000

    Rename NewItem component to NewItemForm

--- frontend/src/components/AddNewItem.js
+++ frontend/src/components/AddNewItem.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import NewItemForm from './NewItem';
+import NewItemForm from './NewItemForm';
 import NewItemList from './NewItemList';
 import { addNewItemToNewItemList } from '../actions';
 
