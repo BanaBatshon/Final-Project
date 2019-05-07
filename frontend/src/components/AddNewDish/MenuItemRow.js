63089ce705d311160a4af541f0933a4175999f4a
Bana <banabatshon@hotmail.com>
2019-05-07 20:07:18 +0000

    Activate the delete button by creating a deletehandler accessing that action

--- frontend/src/components/AddNewDish/MenuItemRow.js
+++ frontend/src/components/AddNewDish/MenuItemRow.js
@@ -1,21 +1,43 @@
 import React from 'react'
+import { Component } from 'react'
+import { removeNewItemToNewItemList } from '../../actions';
+import { connect } from 'react-redux';
 
-const MenuItemRow = (props) => {
+class MenuItemRow extends Component {
+
+  constructor (props) {
+    super (props)
+  }
+  
+  deleteHandler = () => {
+    this.props.dispatch(removeNewItemToNewItemList(this.props.item));
+  }
+
+  render() {
   return (
     <tr>
-    <th scope="row">{ props.index + 1 }</th>
-    <td>{props.name}</td>
+    <th scope="row">{ this.props.index + 1 }</th>
+    <td>{this.props.name}</td>
     <td>
-      {props.tags.map((tag, index) => {
+      {this.props.tags.map((tag, index) => {
         return (
           <span key={ index } className="badge badge-success">{tag.name}</span>
         )})}
     </td>
     <td>
       <button type="button" className="btn btn-secondary btn-sm">Edit</button>
-      <button type="button" className="btn btn-danger btn-sm">Delete</button>
+      <button type="button" onClick={ this.deleteHandler } className="btn btn-danger btn-sm">Delete</button>
     </td>
     </tr>
   )}
+}
+
+const mapStateToProps = state => {
+  return {
+    newItems: state.newItems
+  }
+}
   
-  export default MenuItemRow;
+export default connect(
+  mapStateToProps,
+)(MenuItemRow);
