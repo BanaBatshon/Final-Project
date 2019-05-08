ec17197a1532cc260949c5ce1db9b46425d9ede3
Andrew Li <mail@andrewli.ca>
2019-05-08 17:34:53 +0000

    Remove duplicate footer

--- frontend/src/components/ApproveDishes/index.js
+++ frontend/src/components/ApproveDishes/index.js
@@ -1,13 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import MainView from './MainView';
-import Footer from '../Footer';
 
 class ApproveDishes extends Component {
   render() {
     return (
       <Fragment>
         <MainView />
-        <Footer />
       </Fragment>
     );
   }
